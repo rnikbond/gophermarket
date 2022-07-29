21993be9104b886fd9e282ad25dd1ce23b7453ad
rnikbond <rnikbond@gmail.com>
2022-07-30 01:16:50 +0300

    Добалено создание таблиц в БД.

--- pkg/repository/postgres.go
+++ pkg/repository/postgres.go
@@ -1,6 +1,8 @@
 package repository
 
 import (
+	"fmt"
+
 	"github.com/jmoiron/sqlx"
 )
 
@@ -19,5 +21,20 @@ func NewPostgresDB(dsn string) (*sqlx.DB, error) {
 		return nil, err
 	}
 
+	if err := createTables(db); err != nil {
+		return nil, err
+	}
+
 	return db, nil
+}
+
+func createTables(db *sqlx.DB) error {
+
+	query := fmt.Sprintf(`CREATE TABLE IF NOT EXISTS %s (
+                            id SERIAL PRIMARY KEY,
+		                    username CHARACTER VARYING(50),
+		                    password_hash CHARACTER VARYING(100));`, usersTable)
+
+	_, err := db.Exec(query)
+	return err
 }
