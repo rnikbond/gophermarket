2715488663637491ab687125eb8e0c96452eedfe
rnikbond <rnikbond@gmail.com>
2022-08-09 01:58:11 +0300

    Исправлено преобразование баллов при чтении их базы

--- internal/repository/postgres/loyalty.go
+++ internal/repository/postgres/loyalty.go
@@ -38,7 +38,7 @@ func (l Loyalty) HowMatchAvailable(username string) (float64, error) {
 		return 0, nil
 	}
 
-	accrualsSum := float64(accrualsUser / 100)
+	accrualsSum := float64(accrualsUser) / 100
 	l.logger.Err.Printf("get accruals sum %f for user %s\n", accrualsSum, username)
 
 	return accrualsSum, nil
@@ -59,7 +59,7 @@ func (l Loyalty) HowMatchUsed(username string) (float64, error) {
 func (l Loyalty) SetAccrual(order int64, accrual float64) error {
 
 	accrualRound := int64(accrual * 100)
-	l.logger.Err.Printf("set accrual %d for order %d\n", accrualRound, accrual)
+	l.logger.Err.Printf("set accrual %d for order %d\n", accrualRound, order)
 	_, err := l.db.Exec(queryUpdateAccrual, accrualRound, order)
 	return err
 }
